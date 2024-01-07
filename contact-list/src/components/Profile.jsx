c437cc21840e576aa586c98b77f934869c8a3622
Sahil Kullar <sahil_kullar@outlook.com>
2024-01-07 11:06:50 +0530

    Stub profile page added

---
+++ contact-list/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <h2 className="title">My Profile!!</h2>
+      <p>
+        Hi there, <br />
+        I'm Jhone Doe! <br />
+        This is my phone number: 965-555-xxx <br />
+      </p>
+    </div>
+  );
+}
